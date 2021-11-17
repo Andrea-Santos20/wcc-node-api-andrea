c02e5fccfbaed937c86a4673bb399a9c19e1b883
Andrea Santos <dea0312@gmail.com>
2021-11-17 05:50:21 -0300

    base do sistema de artigos

--- index.js
+++ index.js
@@ -4,37 +4,10 @@ const port = 8080;
 
 app.use(express.json());
 
-app.get("/", function(req, res){
-    res.send("Minha primeira requisição.");
+app.get("/", function(req, res) {
+    res.send("Dasa Educa - Artigos");
 });
 
-app.get("/segunda-req", function(req, res){
-    res.send("Minha Segunda requisição");
+app.listen(port, function() {
+    console.log("Ouvindo a porta: ", port);
 });
-
-app.get("/com-parametros", function(req, res) {
-    if(req.query.nome === "Andrea"){
-        res.send ("Andrea chamou requisição")
-    }
-    res.send("Com parametros funciona! Sabadou " + req.query.nome);
-});
-
-app.post("/meu-primeiro-post", function(req, res) {
-    console.log(req.body);
-    res.send("Meu post funcionou!")
-});
-
-app.put("/meu-primeiro-put/:id", function(req, res) {
-    console.log(req.body, req.params.id);
-    res.send("Meu pout funcionou!")
-});
-
-app.delete("/meu-primeiro-delete/:id", function(req, res) {
-    console.log(req.params.id);
-    res.send("Meu delete funcionou! " + req.params.id)
-});
-
-app.listen(port, function(){
-    console.log("ouvindo a porta: ", port);
-});
-
