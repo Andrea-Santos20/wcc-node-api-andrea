ce45bf3e29651ebb6fdd8f5fa549b3dd0870401c
Andrea Santos <dea0312@gmail.com>
2021-11-17 05:48:05 -0300

    minhas primeiras rotas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
 app.get("/", function(req, res){
     res.send("Minha primeira requisição.");
 });
@@ -17,21 +19,20 @@ app.get("/com-parametros", function(req, res) {
     res.send("Com parametros funciona! Sabadou " + req.query.nome);
 });
 
-app.post("/meu-primeiro-post", function (req, res) {
+app.post("/meu-primeiro-post", function(req, res) {
     console.log(req.body);
     res.send("Meu post funcionou!")
 });
 
-app.put("/meu-primeiro-put/:id", function (req, res) {
+app.put("/meu-primeiro-put/:id", function(req, res) {
     console.log(req.body, req.params.id);
     res.send("Meu pout funcionou!")
 });
 
-app.delete("/meu-primeiro-delete/:id", function (req, res) {
+app.delete("/meu-primeiro-delete/:id", function(req, res) {
     console.log(req.params.id);
     res.send("Meu delete funcionou! " + req.params.id)
 });
-
 
 app.listen(port, function(){
     console.log("ouvindo a porta: ", port);
